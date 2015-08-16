314d660b57f83522856e9b351d4add8299110c1b
Tom Ridd <twridd@gmail.com>
2015-08-16 20:44:00 +0100

    UriEndsWith query added and tested

--- src/test/java/com/github/thomasridd/flatsy/query/FlatsyCursorTest.java
+++ src/test/java/com/github/thomasridd/flatsy/query/FlatsyCursorTest.java
@@ -3,6 +3,7 @@ package com.github.thomasridd.flatsy.query;
 import com.github.thomasridd.flatsy.Builder;
 import com.github.thomasridd.flatsy.FlatsyDatabase;
 import com.github.thomasridd.flatsy.FlatsyFlatFileDatabase;
+import com.github.thomasridd.flatsy.FlatsyObject;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
@@ -89,7 +90,7 @@ public class FlatsyCursorTest {
         // we expect something non null
         cursor.next();
         assertEquals("beta", cursor.currentObject().uri);
-        assertTrue( cursor.next() );
+        assertTrue(cursor.next());
         assertEquals("beta/four.json", cursor.currentObject().uri);
         assertTrue(cursor.next());
         assertEquals("beta/three.json", cursor.currentObject().uri);
@@ -97,4 +98,22 @@ public class FlatsyCursorTest {
 
     }
 
+    @Test
+    public void endsWithQuery_givenDatabase_shouldReturnExpectedResults() {
+        // Given
+        // a database system and a simple query (that should bring results)
+        FlatsyDatabase db = new FlatsyFlatFileDatabase(root);
+        db.create(new FlatsyObject("test/data/data/data.razzle", db), "test item");
+        FlatsyQuery query = new FlatsyUriEndsWith(".razzle");
+
+        // When
+        // we create a cursor
+        FlatsyCursor cursor = new FlatsyCursor(db.rootObject(), query);
+
+        // Then
+        // we expect something non null
+        assertTrue(cursor.next());
+        assertEquals("test/data/data/data.razzle", cursor.currentObject().uri);
+        assertFalse(cursor.next());
+    }
 }

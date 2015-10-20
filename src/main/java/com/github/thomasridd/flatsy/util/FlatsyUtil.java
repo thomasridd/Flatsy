ea0fe4a64870caf614a36c192d1233988c025c0e
Thomas Ridd <twridd@gmail.com>
2015-10-20 12:23:46 +0100

    String builder

--- src/main/java/com/github/thomasridd/flatsy/util/FlatsyUtil.java
+++ src/main/java/com/github/thomasridd/flatsy/util/FlatsyUtil.java
@@ -1,5 +1,10 @@
 package com.github.thomasridd.flatsy.util;
 
+import com.github.thomasridd.flatsy.FlatsyObject;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -13,7 +18,7 @@ import java.util.regex.Pattern;
 public class FlatsyUtil {
 
     /**
-     * Split a command line into
+     * Split a command line into arguments
      *
      * @param command a space separated string where substrings with spaces are contained in " quotes
      * @return a list of substrings
@@ -27,4 +32,51 @@ public class FlatsyUtil {
         return list;
     }
 
+    /**
+     * Build a string using a primitive string builder
+     *
+     * Syntax
+     * plaintext + \"plain text\" + ~.uri + $.jsonpath
+     *
+     * ++ = single space
+     *
+     * @param expression an expression
+     * @param object
+     * @return
+     * @throws IOException
+     */
+    public static String stringOperation(String expression, FlatsyObject object) throws IOException {
+        List<String> list = new ArrayList<>();
+
+        String replaced = expression.replace("++", "+ \" \" +");
+
+        // hideous regex splits on +'s outside quotation marks and then does a bit of trimming
+        String[] split = replaced.split("\\+(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)");
+        for (String item: split) { list.add(item.trim().replace("\"", "")); }
+
+        String result = "";
+        for(String item: list) {
+            if (item.equalsIgnoreCase("~.uri")) {
+                // add the object uri
+                result += object.uri;
+
+            } else if(item.startsWith("$.")) {
+                // add a jsonpath string
+                DocumentContext context = JsonPath.parse(object.retrieveStream());
+                try {
+                    String value = context.read(item);
+                    value = value.replace("\n", " ").replace("\r", " ");
+                    result += value;
+                } catch (com.jayway.jsonpath.PathNotFoundException e) {
+                    result += ".";
+                }
+
+            } else {
+                // straight string
+                result += item;
+
+            }
+        }
+        return result;
+    }
 }
---
+++ src/test/java/com/github/thomasridd/flatsy/util/FlatsyUtilTest.java
@@ -0,0 +1,100 @@
+package com.github.thomasridd.flatsy.util;
+
+import com.github.thomasridd.flatsy.Builder;
+import com.github.thomasridd.flatsy.FlatsyDatabase;
+import com.github.thomasridd.flatsy.FlatsyFlatFileDatabase;
+import com.github.thomasridd.flatsy.FlatsyObject;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by thomasridd on 20/10/15.
+ */
+public class FlatsyUtilTest {
+    @Test
+    public void commandLineParser_givenMixedLine_returnsAppropriateArguments() {
+        // Given
+        // a command line with a mixture of arguments
+        String command = "\"Flat 4\" 86 Greencroft  gardens";
+
+        // When
+        // it is parsed
+        List<String> strings = FlatsyUtil.commandArguments(command);
+
+        // Then
+        // we get appropriate arguments returned
+        assertEquals("Flat 4", strings.get(0));
+        assertEquals("86", strings.get(1));
+        assertEquals("Greencroft", strings.get(2));
+        assertEquals("gardens", strings.get(3));
+    }
+
+    @Test
+    public void stringBuilder_givenSimpleExpression_returnsExpectedResult() throws IOException {
+        // Given
+        // a simple expression
+        String expression = "Black + berry";
+
+        // When
+        // we build a string
+        String result = FlatsyUtil.stringOperation(expression, null);
+
+        // Then
+        // it returns the expected result
+        assertEquals("Blackberry", result);
+    }
+
+    @Test
+    public void stringBuilder_withDoublePlus_addsSingleSpace() throws IOException {
+        // Given
+        // a simple expression
+        String expression = "Black ++ berry";
+
+        // When
+        // we build a string
+        String result = FlatsyUtil.stringOperation(expression, null);
+
+        // Then
+        // it returns the expected result
+        assertEquals("Black berry", result);
+    }
+
+    @Test
+    public void stringBuilder_withFlatsyObject_addsUri() throws IOException {
+        // Given
+        // a simple expression
+        String expression = "URI= + ~.uri";
+        FlatsyDatabase db = new FlatsyFlatFileDatabase(Builder.emptyTestDatabase());
+        FlatsyObject object = new FlatsyObject("test/object", db);
+
+        // When
+        // we build a string
+        String result = FlatsyUtil.stringOperation(expression, object);
+
+        // Then
+        // it returns the expected result
+        assertEquals("URI=test/object", result);
+    }
+
+    @Test
+    public void stringBuilder_withJSONObject_addsJSONPath() throws IOException {
+        // Given
+        // a simple expression
+        String expression = "Title: ++ $.description.title";
+        FlatsyDatabase db = new FlatsyFlatFileDatabase(Builder.copyFlatFiles());
+
+        FlatsyObject object = new FlatsyObject("births/data.json", db);
+
+        // When
+        // we build a string
+        String result = FlatsyUtil.stringOperation(expression, object);
+
+        // Then
+        // it returns the expected result
+        assertEquals("Title: Births, deaths and marriages", result);
+    }
+}

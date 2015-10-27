1d2c6e0259652b21e163662757a1a141a39b595a
Thomas Ridd <twridd@gmail.com>
2015-10-27 10:01:36 +0000

    Scripting brought forward

--- src/main/java/com/github/thomasridd/flatsy/Scripts.java
+++ src/main/java/com/github/thomasridd/flatsy/Scripts.java
@@ -1,23 +1,154 @@
 package com.github.thomasridd.flatsy;
 
-import java.nio.file.Path;
+import com.github.onsdigital.zebedee.content.page.statistics.dataset.Dataset;
+import com.github.onsdigital.zebedee.content.page.statistics.dataset.DatasetLandingPage;
+import com.github.onsdigital.zebedee.content.page.statistics.dataset.DownloadSection;
+import com.github.onsdigital.zebedee.content.partial.Link;
+import com.github.onsdigital.zebedee.content.util.ContentUtil;
+import com.github.thomasridd.flatsy.operations.operators.CopyTo;
+import com.github.thomasridd.flatsy.operations.operators.Delete;
+import com.github.thomasridd.flatsy.operations.operators.Replace;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by thomasridd on 23/10/15.
  */
 public class Scripts {
 
-
-
-    public static FlatsyCommandLine commandLineForDatasets(String root) {
+    public static List<FlatsyObject> datasetFiles(String root) {
         FlatsyCommandLine cli = new FlatsyCommandLine();
         cli.runCommand("from " + root);
-        cli.runCommand("filter uri_ends /datasets");
-        cli.runCommand("filter folders");
+        cli.runCommand("filter uri_contains /datasets/");
+        cli.runCommand("filter uri_ends /data.json");
+        cli.runCommand("filter jsonpath $.type equals dataset");
+        List<FlatsyObject> all = cli.cursor().getAll();
 
-        return cli;
+        List<FlatsyObject> doUpdate = new ArrayList<>();
+        for (FlatsyObject obj: all) {
+            if (obj.parent().parent().uri.endsWith("/datasets")) {
+                doUpdate.add(obj);
+            }
+        }
+
+        return doUpdate;
     }
 
+    public static boolean copyDatasetToSubfolders(FlatsyObject datasetObj) throws IOException, URISyntaxException {
+        try (InputStream stream = datasetObj.retrieveStream()) {
+
+            Delete delete = new Delete(datasetObj.db);
+
+
+            Dataset dataset = ContentUtil.deserialise(stream, Dataset.class);
+            for (DownloadSection download: dataset.getDownloads()) {
+
+                FlatsyObject file = datasetObj.db.get(download.getFile());
+                String folder = cleanString(download.getTitle());
+
+                CopyTo copyTo = new CopyTo(datasetObj.db, "~.parent + /" + folder + "/ + ~.file");
+                copyTo.apply(datasetObj);
+                copyTo.apply(file);
+
+                delete.apply(file);
+            }
+
+            // Update uri's in files
+            List<FlatsyObject> subfiles = subDatasets(datasetObj);
+            for (FlatsyObject subfile: subfiles) {
+                Replace replace = new Replace(datasetObj.parent().uri, datasetObj.parent().uri + "/" + subfile.parent().uri);
+                replace.apply(subfile);
+
+                // Strip out all downloads except their own file
+                reformatSubdatasetDownloads(subfile, datasetObj.db);
+            }
+        }
+
+        DatasetLandingPage landingPage;
+        try (InputStream stream = datasetObj.retrieveStream()) {
+
+
+
+            landingPage = ContentUtil.deserialise(stream, DatasetLandingPage.class);
+            List<Link> datasets = new ArrayList<>();
+
+            for (FlatsyObject object: datasetObj.parent().children()) {
+                if (object.getType() == FlatsyObjectType.Folder) {
+                    datasets.add( new Link(new URI(object.uri)));
+                }
+            }
+
+            landingPage.setDownloads(null);
+            landingPage.setDatasets(datasets);
+        }
+        Delete delete = new Delete(datasetObj.db);
+        delete.apply(datasetObj);
+        datasetObj.create(ContentUtil.serialise(landingPage));
+
+        return  false;
+    }
+
+    public static List<FlatsyObject> subDatasets(FlatsyObject parent) {
+
+        FlatsyCommandLine cli = new FlatsyCommandLine();
+        cli.runCommand("from " + parent.db.toString() + "/" + parent.parent().uri);
+        cli.runCommand("filter uri_ends /data.json");
+        cli.runCommand("filter jsonpath $.type equals dataset");
+        List<FlatsyObject> all = cli.cursor().getAll();
+
+        List<FlatsyObject> doUpdate = new ArrayList<>();
+        for (FlatsyObject obj: all) {
+            if (!obj.parent().parent().uri.endsWith("/datasets")) {
+                doUpdate.add(obj);
+            }
+        }
+
+        return doUpdate;
+    }
+
+    public static void reformatSubdatasetDownloads(FlatsyObject subdataset, FlatsyDatabase root) {
+        try(InputStream stream = subdataset.retrieveStream()) {
+            Dataset dataset = ContentUtil.deserialise(stream, Dataset.class);
+            List<DownloadSection> downloads = dataset.getDownloads();
+            List<DownloadSection> newDownloads = new ArrayList<>();
+
+            // Update the downloads section for all files that exist
+            for (DownloadSection section: downloads) {
+                if (root.get(section.getFile()).getType() != FlatsyObjectType.Null) {
+                    newDownloads.add(section);
+                }
+            }
+            dataset.setDownloads(newDownloads);
+
+            subdataset.create(ContentUtil.serialise(dataset));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String cleanString(String string) {
+        final int MAX_LENGTH = 30;
+
+        StringBuilder filename = new StringBuilder();
+
+        // Strip dodgy characters:
+        for (char c : string.toCharArray()) {
+            if (c == '.' || Character.isJavaIdentifierPart(c)) {
+                filename.append(c);
+            }
+        }
+
+        // Ensure the String is a sensible length:
+        return StringUtils.lowerCase(StringUtils.abbreviateMiddle(filename.toString(), "_",
+                MAX_LENGTH));
+    }
 
     public static void main(String[] args) {
 
--- src/test/java/com/github/thomasridd/flatsy/ScriptsTest.java
+++ src/test/java/com/github/thomasridd/flatsy/ScriptsTest.java
@@ -1,11 +1,17 @@
 package com.github.thomasridd.flatsy;
 
+import com.github.thomasridd.flatsy.query.FlatsyCursor;
+import com.github.thomasridd.flatsy.query.matchers.MatcherCommandLineParser;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -28,15 +34,38 @@ public class ScriptsTest {
     }
 
     @Test
-    public void __() {
+    public void commandLineForDatasets_givenFolderWithDatasets_returnsResults() throws IOException {
         // Given
-        // 
+        // a database
+        FlatsyDatabase db = new FlatsyFlatFileDatabase(root);
 
         // When
-        // 
+        // we get our script of datasetFiles
+        List<FlatsyObject> flatsyObjects = Scripts.datasetFiles(root.toString());
 
         // Then
-        // 
+        // we should get a list of files
+        assertNotEquals(0, flatsyObjects.size());
+
+    }
+
+    @Test
+    public void copyDatasetToSubfolders_givenFolderWithDatasets_movesFiles() throws IOException, URISyntaxException {
+        // Given
+        // a database
+        FlatsyDatabase db = new FlatsyFlatFileDatabase(root);
+
+        // When
+        // we get our script of datasetFiles
+        List<FlatsyObject> flatsyObjects = Scripts.datasetFiles(root.toString());
+        for (FlatsyObject obj: flatsyObjects) {
+            Scripts.copyDatasetToSubfolders(obj);
+        }
+
+        System.out.println(root.toString());
+        // Then
+        // we should get a list of files
+        assertNotEquals(0, flatsyObjects.size());
 
     }
 }
